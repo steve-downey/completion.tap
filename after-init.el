574125b63fa85893744b3d4405427eca86b25c8f
Steve Downey <sdowney2@bloomberg.net>
2024-09-21 15:08:33 -0400

    more config

--- after-init.el
+++ after-init.el
@@ -16,6 +16,10 @@
   :bind (:map minibuffer-local-map
               ("M-A" . marginalia-cycle))
 
+  :custom
+  (marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light nil))
+  (marginalia-max-relative-age 0)
+  (marginalia-align 'right)
   ;; The :init section is always executed.
   :init
 
@@ -26,12 +30,10 @@
   )
 
 (use-package orderless
-  :ensure t
   :config
   (setq completion-styles '(orderless basic)))
 
 (use-package consult
-  :ensure t
   :bind (;; A recursive grep
          ("M-s M-g" . consult-grep)
          ;; Search for files names recursively
@@ -45,17 +47,14 @@
          ("M-s M-b" . consult-buffer)))
 
 (use-package embark
-  :ensure t
   :bind (("C-." . embark-act)
          :map minibuffer-local-map
          ("C-c C-c" . embark-collect)
          ("C-c C-e" . embark-export)))
 
-(use-package embark-consult
-  :ensure t)
+(use-package embark-consult)
 
 (use-package wgrep
-  :ensure t
   :bind ( :map grep-mode-map
           ("e" . wgrep-change-to-wgrep-mode)
           ("C-x C-q" . wgrep-change-to-wgrep-mode)
