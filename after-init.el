a2bd98f1f4572e9623c7cf11058af8cf6778118f
Steve Downey <sdowney2@bloomberg.net>
2024-09-24 17:35:10 -0400

    Corfu and cape
    Plus twaeks

--- after-init.el
+++ after-init.el
@@ -1,6 +1,13 @@
 ;; -*- lexical-binding: t -*-
 
 (use-package vertico
+  :demand t
+  :bind  (:map vertico-map
+               ("?" . minibuffer-completion-help)
+               ("M-RET" . minibuffer-force-complete-and-exit)
+               ("M-TAB" . minibuffer-complete)
+               ("C-n" . vertico-next)
+               ("C-p" . vertico-previous))
   :custom
   (vertico-count 13)                    ; Number of candidates to display
   (vertico-resize t)
@@ -13,6 +20,7 @@
   ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
   ;; available in the *Completions* buffer, add it to the
   ;; `completion-list-mode-map'.
+  :demand t
   :bind (:map minibuffer-local-map
               ("M-A" . marginalia-cycle))
 
@@ -30,10 +38,13 @@
   )
 
 (use-package orderless
+  :demand t
   :config
   (setq completion-styles '(orderless basic)))
 
 (use-package consult
+  :demand t
+  :hook (completion-list-mode . consult-preview-at-point-mode)
   :bind (;; A recursive grep
          ("M-s M-g" . consult-grep)
          ;; Search for files names recursively
@@ -44,9 +55,14 @@
          ("M-s M-l" . consult-line)
          ;; Switch to another buffer, or bookmarked file, or recently
          ;; opened file.
-         ("M-s M-b" . consult-buffer)))
+         ("M-s M-b" . consult-buffer))
+  :config
+  (setq consult-line-numbers-widen t)
+  (setq consult-preview-key 'any)
+  )
 
 (use-package embark
+  :demand t
   :bind (("C-." . embark-act)
          :map minibuffer-local-map
          ("C-c C-c" . embark-collect)
@@ -55,6 +71,7 @@
 (use-package embark-consult)
 
 (use-package wgrep
+  :demand t
   :bind ( :map grep-mode-map
           ("e" . wgrep-change-to-wgrep-mode)
           ("C-x C-q" . wgrep-change-to-wgrep-mode)
@@ -66,6 +83,73 @@
 
 (use-package all-the-icons-completion
   :after (marginalia all-the-icons)
+  :demand t
   :hook (marginalia-mode . all-the-icons-completion-marginalia-setup)
   :init
   (all-the-icons-completion-mode))
+
+(use-package corfu
+  :bind ( :map corfu-map
+            ("C-n" . corfu-next)
+            ("C-p" . corfu-previous)
+            ("M-d" . corfu-show-documentation)
+            ("M-l" . corfu-show-location))
+  :custom
+  (corfu-auto nil)        ; Only use `corfu' when calling `completion-at-point' or
+                                        ; `indent-for-tab-command'
+  (corfu-auto-prefix 2)
+  (corfu-auto-delay 0.25)
+
+  (corfu-min-width 80)
+  (corfu-max-width corfu-min-width)       ; Always have the same width
+  (corfu-count 14)
+  (corfu-scroll-margin 4)
+  (corfu-cycle nil)
+
+  ;; `nil' means to ignore `corfu-separator' behavior, that is, use the older
+  ;; `corfu-quit-at-boundary' = nil behavior. Set this to separator if using
+  ;; `corfu-auto' = `t' workflow (in that case, make sure you also set up
+  ;; `corfu-separator' and a keybind for `corfu-insert-separator', which my
+  ;; configuration already has pre-prepared). Necessary for manual corfu usage with
+  ;; orderless, otherwise first component is ignored, unless `corfu-separator'
+  ;; is inserted.
+  (corfu-quit-at-boundary nil)
+  (corfu-preselect-first t)        ; Preselect first candidate?
+
+  ;; Other
+  ;; NOTE 2022-02-05: In my actual configuration, I have this variable set to nil
+  ;; since I use `corfu-doc', whose configuration comes later. But if you don't
+  ;; use `corfu-doc', this might be helpful to you.
+  (corfu-echo-documentation t)            ; Show documentation in echo area?
+  :config
+  (corfu-global-mode))
+
+(use-package kind-icon
+  :ensure t
+  :after corfu
+  :custom
+  (kind-icon-blend-background t)
+  (kind-icon-default-face 'corfu-default) ; only needed with blend-background
+  :config
+  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+
+(use-package cape
+  ;; Bind prefix keymap providing all Cape commands under a mnemonic key.
+  ;; Press C-c p ? to for help.
+  :bind ("C-c p" . cape-prefix-map) ;; Alternative keys: M-p, M-+, ...
+  ;; Alternatively bind Cape commands individually.
+  ;; :bind (("C-c p d" . cape-dabbrev)
+  ;;        ("C-c p h" . cape-history)
+  ;;        ("C-c p f" . cape-file)
+  ;;        ...)
+  :init
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+  (add-hook 'completion-at-point-functions #'cape-dabbrev)
+  (add-hook 'completion-at-point-functions #'cape-file)
+  (add-hook 'completion-at-point-functions #'cape-elisp-block)
+  ;; (add-hook 'completion-at-point-functions #'cape-history)
+  ;; ...
+  )
